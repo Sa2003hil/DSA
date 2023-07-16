fe5c0a845fee08a98728588efe3c77547ce97bf6
Sahil <Sahil2003@gmail.com>
2023-07-16 22:31:01 +0530

    files upload

---
+++ Introdunction to CPP/Leet Code/Reverse_array.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> reverse(vector<int> v)
+{
+    int s = 0, e = v.size() - 1;
+
+    while (s <= e)
+    {
+        swap(v[s], v[e]);
+        s++;
+        e--;
+    }
+    return v;
+}
+
+void print(vector<int> v)
+{
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    vector<int> ans = reverse(v);
+
+    cout << "Printing Reverse vector" << endl;
+    print(ans);
+
+    return 0;
+}
---
+++ Introdunction to CPP/STL/LUV cp Playlist/Maps/Maps/Basics.cpp
@@ -0,0 +1,44 @@
+/*
+
+Each element has a key value and a mapped value. No two mapped values can have the same key values.
+
+Maps are in sorted order and they are in non-contigous manner so that's why we can't do it+1 we have to do it++ .... internal implementation of maps is done using Red Black Trees
+
+
+
+Time Complexity :- TC of adding value in the map is O(log(n))---> where n is
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    map<int, string> m;
+
+    // method 1 to insert
+    m[1] = "abc";
+    m[5] = "cdc";
+    m[3] = "acd";
+
+    // method 2 to insert
+    m.insert({3, "afd"});
+
+    // printing the{ key : value} pairs using iterator method
+
+    map<int, string>::iterator it;
+
+    for (it = m.begin(); it != m.end(); ++it)
+    {
+        cout << (*it).first << " " << (*it).second << endl;
+    }
+
+    // using range based loops and auto method ( Shortcut )
+
+    for (auto &pr : m)
+    {
+        cout << pr.first << " " << pr.second << endl;
+    }
+
+    return 0;
+}
