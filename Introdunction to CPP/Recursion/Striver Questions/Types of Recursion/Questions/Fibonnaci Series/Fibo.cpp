ab1ee4a221af36015e22d648b3417be587a0556a
Sahil <Sahil2003@gmail.com>
2023-10-18 16:51:32 +0530

    Multiple recurssion calls concept added

--- Introdunction to CPP/Recursion/Striver Questions/Types of Recursion/Questions/Fibonnaci Series/Fibo.cpp
+++ Introdunction to CPP/Recursion/Striver Questions/Types of Recursion/Questions/Fibonnaci Series/Fibo.cpp
@@ -1,16 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int fibo(int n){
-    if(n==0){
+int findfibo(int n)
+{
+    // if(n==0 || n==1){
+    //     return n;
+    // }
+    // else{
+    //     return findfibo(n-1)+findfibo(n-2);
+    // }
+
+    // this is using recursion (multiple recursion)
+    if (n <= 1)
         return n;
+    int last = findfibo(n - 1);
+    int slast = findfibo(n - 2);
+    return last + slast;
+}
+
+vector<int> generateFibonacciNumbers(int n)
+{
+    // Write your code here.
+    vector<int> findseq;
+    for (int i = 0; i < n; i++)
+    {
+        findseq.push_back(findfibo(i));
     }
-    return 
+    return findseq;
 }
 
 int main(int argc, char const *argv[])
 {
-    int n;
-    cin>>n;
-    return 0;
 }
