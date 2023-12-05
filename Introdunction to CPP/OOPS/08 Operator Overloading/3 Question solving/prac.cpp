3ec0668df4c19b1aecb931f157b28ffc801eca93
Sahil <Sahil2003@gmail.com>
2023-12-05 11:56:46 +0530

    practise codechef string questions

--- Introdunction to CPP/OOPS/08 Operator Overloading/3 Question solving/prac.cpp
+++ Introdunction to CPP/OOPS/08 Operator Overloading/3 Question solving/prac.cpp
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    int choice;
-    cin >> choice;
+    int k, n;
+    cin >> k >> n;
+    string a[k], b[n];
 
-    int leng, bred;
-    float res;
-    switch (choice)
+    for (int i = 0; i < k; i++)
+        cin >> a[i]; // TC:- O(n)
+    for (int j = 0; j < n; j++)
+        cin >> b[j]; // TC :- O(n)
+
+    // now compare all elements of b
+    for (int i = 0; i < n; i++)
     {
-    case 1:
-        cin >> leng >> bred;
-        res = leng * bred;
-        cout << res << endl;
-        break;
-
-    case 2:
-        int radi;
-        cin >> radi;
-        res = 3.17 * radi * radi;
-        cout << res << endl;
-    default:
-        break;
+        int check = 1;
+        // check for each element in the array a
+        for (int j = 0; j < k; j++)
+        {
+            if (b[i].find(a[j]) != -1)
+            {
+                cout << "Good" << endl;
+            }
+        }
+        if (b[i].length() >= 47)
+        {
+            cout << "Good" << endl;
+        }
+        else
+        {
+            cout << "Bad" << endl;
+        }
     }
+
     return 0;
 }
