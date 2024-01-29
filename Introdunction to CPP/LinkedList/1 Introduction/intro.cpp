2c8981d5e2636518d2873ed966343311a8d6d03f
Sahil <Sahil2003@gmail.com>
2024-01-29 15:09:28 +0530

    Linked List intro added

---
+++ Introdunction to CPP/LinkedList/1 Introduction/intro.cpp
@@ -0,0 +1,19 @@
+/*
+
+What is Linked List :
+Linked List is a linear data structure -> collection of nodes. Unlike arrays, linked list elements are not stored at a contiguous location; the elements are linked using pointers.
+
+Node : Each node of a list is made up of two items - the data and a reference to the next node. The last node has a reference to null. The entry point into a linked list is called the head of the list.
+
+Why Linked List is needed ?
+
+example : arr[10] ---> can we change the size of the array like 100 in runtime ? no.
+
+In case of vectors we can do that but it is not efficient.
+
+So to overcome this problem we use linked list.It is the dynamic data structure which can grow and shrink during runtime, means no memory is wasted
+
+
+Insertion/Deleteion requires no shifting of elements, only the adjustment of the pointer. unlike arrays where shifting is required.
+
+*/
