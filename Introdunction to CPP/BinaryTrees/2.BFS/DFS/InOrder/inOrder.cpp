396dc0d6f4ddb567eb5a18b25414fe664bf80ab4
Sahil <Sahil2003@gmail.com>
2024-05-10 17:34:53 +0530

    Binary trees traversal done

--- Introdunction to CPP/BinaryTrees/2.BFS/DFS/InOrder/inOrder.cpp
+++ Introdunction to CPP/BinaryTrees/2.BFS/DFS/InOrder/inOrder.cpp
@@ -1,11 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
 
-    int
-    main(int argc, char const *argv[])
+    // creating a constructor
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the data" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    // constructor will used to create a new node
+
+    if (data == -1)
+        return NULL;
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void inOrder(node *root)
+// L N R
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void preOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+int main(int argc, char const *argv[])
 {
     /* code */
+    node *root = NULL;
+    root = buildTree(root);
+    inOrder(root);
+    cout << endl;
+    preOrder(root);
+    cout << endl;
+    postOrder(root);
     return 0;
 }
+
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
